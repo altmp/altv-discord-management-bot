ed9fda6659b230e5d58cd2c3b041558459430a1d
Zack <zackary12389@yahoo.co.uk>
2021-08-03 17:02:09 +0100

    Add wiki command

---
+++ src/commands/wiki.ts
@@ -0,0 +1,17 @@
+import * as Discord from 'discord.js';
+import generateEmbed from '../utility/embed'
+
+import { ICommand } from '../interfaces/ICommand';
+
+const command: ICommand = {
+    command: 'wiki',
+    description: 'Embed link to the wiki.',
+    execute: async (msg: Discord.Message) => {
+        const embed = generateEmbed("Check the Wiki!", "https://wiki.altv.mp/wiki/Main_Page")
+        embed.thumbnail = { url: "https://i.imgur.com/gCfdh2c.png" };
+
+        msg.reply(embed);
+    },
+}
+
+export default command;
