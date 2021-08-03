cdaf8644631a657707a3afa908eb5fbd29061aba
Tim <chipsleten@gmail.com>
2021-08-03 17:55:27 +0200

    basic note command

---
+++ src/commands/note.ts
@@ -0,0 +1,27 @@
+﻿import * as Discord from 'discord.js';
+import { ICommand } from '../interfaces/ICommand';
+import generateEmbed from "../utility/embed";
+
+const command: ICommand = {
+    command: 'note',
+    description: '!note [name] - Returns a predefined note from an array about specific functionality. Similar to how !support works.',
+    execute: async (msg: Discord.Message) => {
+        
+        if(msg.content.split(' ').length <= 1 || msg.content.split(' ').length > 2) {
+            await msg.reply("Wrong usage! -> !note <name> | !note <add/del> <name>");
+        }
+        
+        switch (msg.content.split(' ')[1]) {
+            case 'test':
+                const testEmbed = generateEmbed(`Test`, 'Test');
+                await msg.reply(testEmbed);
+                break;
+            case 'role':
+                const roleEmbed = generateEmbed(`Role`, 'Role');
+                await msg.reply(roleEmbed);
+                break;
+        }
+    }
+}
+
+export default command;
