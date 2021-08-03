f66bcbd85308b3b5f4227873698a0f055bde96e8
Zack <zackary12389@yahoo.co.uk>
2021-08-03 17:49:48 +0100

    Added search command

---
+++ src/commands/search.ts
@@ -0,0 +1,20 @@
+import * as Discord from 'discord.js';
+import generateEmbed from '../utility/embed';
+
+import { ICommand } from '../interfaces/ICommand';
+
+const command: ICommand = {
+    command: 'search',
+    description: 'Embed telling someone to search Discord.',
+    execute: async (msg: Discord.Message) => {
+        const embed = generateEmbed(
+            'Searching Discord',
+            'Experts and newbies agree that the Discord search will most often answer your question.'
+        );
+        embed.setImage('https://i.imgur.com/tXOGCQJ.png');
+
+        msg.reply(embed);
+    },
+};
+
+export default command;
