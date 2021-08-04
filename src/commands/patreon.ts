75a2ac8bbf29bc50e60ab5b32cf0cab5f0566df5
Zack <zackary12389@yahoo.co.uk>
2021-08-04 22:38:35 +0100

    Add patreon command

---
+++ src/commands/patreon.ts
@@ -0,0 +1,19 @@
+import * as Discord from 'discord.js';
+
+import { ICommand } from '../interfaces/ICommand';
+import generateEmbed from '../utility/embed';
+
+const command: ICommand = {
+    command: 'patreon',
+    description: 'Link to Patreon.',
+    execute: async (msg: Discord.Message) => {
+        const embed = generateEmbed(
+            'Support the Development of alt:V',
+            'The developers behind alt:V work very hard on ensuring you get the best experience while playing on alt:V. If you wish to support the work that goes into alt:V please consider becoming a Patreon Subscriber!\n https://www.patreon.com/altVMP'
+        );
+
+        msg.reply(embed);
+    },
+};
+
+export default command;
