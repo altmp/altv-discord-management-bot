82ec8a0193e704bbdd478ad9bf107fe2be5cd57b
chaosfreak93 <yanluca.lentzen@gmail.com>
2021-08-04 21:46:05 +0200

    Cleanup Lockdown System

--- src/commands/lockdown.ts
+++ src/commands/lockdown.ts
@@ -1,4 +1,5 @@
 import * as Discord from 'discord.js';
+import ms from 'ms';
 import { getGuild } from '..';
 
 import { ICommand } from '../interfaces/ICommand';
@@ -10,7 +11,8 @@ import RegexUtility from '../utility/regex';
 const command: ICommand = {
     command: 'lockdown',
     description: '<channel> <minutes> - Locks a Channel.',
-    execute: async (msg: Discord.Message, id: string, minutes: number) => {
+    skipPermissionCheck: true,
+    execute: async (msg: Discord.Message, id: string, time: string) => {
         const channelId = RegexUtility.parseChannelID(id);
         const channel = msg.guild.channels.cache.get(channelId) as Discord.TextChannel;
         if (channel == undefined || channel == null) {
@@ -19,39 +21,29 @@ const command: ICommand = {
         }
 
         const lockdownChannel: ILockdown[] = (await DatabaseService.getData()).lockdownChannel;
-        const search: ILockdown = lockdownChannel.find(x => x.channelId == channel.id);
-
-        if (minutes != undefined || minutes != null) {
-            if (!search) {
-                lockdownChannel.push({ channelId: channel.id, until: Date.now() + (minutes * 60000) });
-                await DatabaseService.updateData({ lockdownChannel });
-            }   
-        }
-        
-        if (search) {
-            const index: number = lockdownChannel.indexOf(search);
-            lockdownChannel.splice(index, 1);
-            await DatabaseService.updateData({ lockdownChannel });
-        }
 
         if (channel.permissionsFor(msg.guild.roles.everyone).has('SEND_MESSAGES')) {
-            if (minutes != undefined || minutes != null) {
-                await channel.send(`🔒 **LOCKED FOR ${minutes} MINUTES** 🔒`)
-                if (channel != msg.channel) msg.channel.send(`🔒 **<#${channel.id}> LOCKED FOR ${minutes} MINUTES** 🔒`);
-            } else {
-                await channel.send("🔒 **LOCKED** 🔒")
-                if (channel != msg.channel) msg.channel.send(`🔒 **<#${channel.id}> LOCKED** 🔒`);
-            }
+            const lockedChannelMessage = time ? `🔒 **LOCKED FOR ${ms(ms(time), { long: true})}** 🔒` : "🔒 **LOCKED** 🔒";
+            const lockedChannelMessage2 = time ? `🔒 **<#${channel.id}> LOCKED FOR ${ms(ms(time), { long: true})}** 🔒` : `🔒 **<#${channel.id}> LOCKED** 🔒`;
+
+            channel.send(lockedChannelMessage);
+            if (channel != msg.channel) msg.channel.send(lockedChannelMessage2);
             setTimeout(() => {
                 channel.updateOverwrite(msg.guild.roles.everyone,{'SEND_MESSAGES': false});
             }, 50);
+            lockdownChannel.push({ channelId: channel.id, until: time ? Date.now() + ms(time) : null });
         } else {
             await channel.updateOverwrite(msg.guild.roles.everyone,{'SEND_MESSAGES': null});
             setTimeout(() => {
                 channel.send("🔓 **UNLOCKED** 🔓");
                 if (channel != msg.channel) msg.channel.send(`🔓 **<#${channel.id}> UNLOCKED** 🔓`);
             }, 50);
+            const search: ILockdown = lockdownChannel.find(x => x.channelId == channel.id);
+            const index: number = lockdownChannel.indexOf(search);
+            lockdownChannel.splice(index, 1);
         }
+
+        await DatabaseService.updateData({ lockdownChannel });
     }
 }
 
@@ -59,7 +51,7 @@ export async function checkLockdownChannel() {
     let lockdownChannel: ILockdown[] = (await DatabaseService.getData()).lockdownChannel;
 
     lockdownChannel.forEach(async (lockDownData) => {
-        if (Date.now() > lockDownData.until) {
+        if (lockDownData.until != null && Date.now() > lockDownData.until) {
             const guild = getGuild();
             const role = guild.roles.everyone;
             const channel = guild.channels.cache.get(lockDownData.channelId) as Discord.TextChannel;
