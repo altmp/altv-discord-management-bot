c10d978ff3e953880d238c349a686b34addcd88d
Zack <zackary12389@yahoo.co.uk>
2021-08-04 22:38:16 +0100

    Add cdn command

---
+++ src/commands/cdn.ts
@@ -0,0 +1,19 @@
+import * as Discord from 'discord.js';
+
+import { ICommand } from '../interfaces/ICommand';
+import generateEmbed from '../utility/embed';
+
+const command: ICommand = {
+    command: 'cdn',
+    description: 'Link to CDN Wiki Information.',
+    execute: async (msg: Discord.Message) => {
+        const embed = generateEmbed('Here is some information about CDN');
+
+        embed.addField('Direct Downloads:', 'https://wiki.altv.mp/wiki/Scripting:CDN_Links');
+        embed.addField('How to Setup a CDN:', 'https://wiki.altv.mp/wiki/Tutorial:Setup_CDN');
+
+        msg.reply(embed);
+    },
+};
+
+export default command;
