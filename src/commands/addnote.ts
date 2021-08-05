c65af0ea75e5704f072a02ce4335fdb8cf51e0aa
Tim <chipsleten@gmail.com>
2021-08-05 18:36:32 +0200

    AddNote command

---
+++ src/commands/addnote.ts
@@ -0,0 +1,26 @@
+﻿import {ICommand} from "../interfaces/ICommand";
+import Discord from "discord.js";
+import {INote} from "../interfaces/INote";
+import {DatabaseService} from "../service/database";
+import generateEmbed from "../utility/embed";
+
+const command: ICommand = {
+    command: 'addnote',
+    description: '!addnote [name] [Content] - Creates a new note from the specified content',
+    skipPermissionCheck: true,
+    execute: async (msg: Discord.Message, name: string) => {
+        const notes: INote[] = (await DatabaseService.getData()).notes;
+        const query: INote = notes.find(note => note.name.toLowerCase() == name.toLowerCase())
+        
+        // Note doesn't exist
+        if (query == null) {
+            await msg.reply(generateEmbed("Error", `Note ${name} does not exist!`));
+            return;
+        } 
+        
+        
+        
+    }
+}
+
+export default command;
