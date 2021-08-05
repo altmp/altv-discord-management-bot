e659093318976d3960ad52e8f493358bc865e231
Tim <chipsleten@gmail.com>
2021-08-05 18:38:10 +0200

    wtf have I done in my last commit [Fix Addnote]

--- src/commands/addnote.ts
+++ src/commands/addnote.ts
@@ -8,15 +8,19 @@ const command: ICommand = {
     command: 'addnote',
     description: '!addnote [name] [Content] - Creates a new note from the specified content',
     skipPermissionCheck: true,
-    execute: async (msg: Discord.Message, name: string) => {
+    execute: async (msg: Discord.Message, name: string, ...content: string[]) => {
         const notes: INote[] = (await DatabaseService.getData()).notes;
         const query: INote = notes.find(note => note.name.toLowerCase() == name.toLowerCase())
-        
-        // Note doesn't exist
-        if (query == null) {
-            await msg.reply(generateEmbed("Error", `Note ${name} does not exist!`));
+
+        // Note exists already
+        if (query != null) {
+            await msg.reply(generateEmbed("Error", "This note already exists!"));
             return;
-        } 
+        }
+
+        notes.push({name: name, response: content.join(' ')})
+        await DatabaseService.updateData({notes: notes});
+        await msg.reply(generateEmbed("Success", `Note ${name} added!`))
         
         
         
